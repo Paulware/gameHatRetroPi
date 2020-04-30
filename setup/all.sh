25681ddcfb5929810301b7f2edecd5ab5ba55925
Paulware <Paulware@hotmail.com>
2020-04-30 08:21:29 -0500

    keyboard.sh

--- setup/all.sh
+++ setup/all.sh
@@ -1,7 +1,5 @@
-sudo apt-get update
-wget https://www.waveshare.com/w/upload/b/b4/Game-HAT-180720.tar.gz
-tar -xvzf Game-HAT-180720.tar.gz
-cd Game_HAT
-sudo ./Game-HAT
-
-echo "/dev/sda1 /media/usb1 auto defaults 0 0" >> /etc/fstab
+./ap.sh
+#sudo apt-get update
+#sudo ./Game-HAT
+./keyboard.sh
+# echo "/dev/sda1 /media/usb1 auto defaults 0 0" >> /etc/fstab
